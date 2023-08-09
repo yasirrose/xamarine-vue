45ef7632101025723ec64d90fe9d4a3ba3bd6052
Yasir Shaukat <yasirzinbox@gmail.com>
2023-08-09 13:23:00 +0500

    Add appointment table

--- src/components/Navbars/AdminNavbar.vue
+++ src/components/Navbars/AdminNavbar.vue
@@ -7,12 +7,12 @@
       class="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4"
     >
       <!-- Brand -->
-      <a
+      <!-- <a
         class="text-white text-sm uppercase hidden lg:inline-block font-semibold"
         href="javascript:void(0)"
       >
         Dashboard
-      </a>
+      </a> -->
       <!-- Form -->
       <form
         class="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3"
