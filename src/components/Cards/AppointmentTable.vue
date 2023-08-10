d04cc190eb52ff497148fc154fb99cc2da615e43
Yasir Shaukat <yasirzinbox@gmail.com>
2023-08-10 20:24:42 +0500

    Add table in appointment

--- src/components/Cards/AppointmentTable.vue
+++ src/components/Cards/AppointmentTable.vue
@@ -1,23 +1,28 @@
 <template>
   <div class="appointments-sec">
-    <h3>Appointments</h3>
-    <div style="width: 100%; flex: 1 1 auto;">
-      <ag-grid-vue
-      style="width: 100%; height: 500px"
-        class="ag-theme-alpine"
-        :columnDefs="columnDefs"
-        :rowData="rowData">
-      </ag-grid-vue>
-    </div>
+    <v-card>
+      <template v-slot:title> Appointments </template>
+
+      <!-- <template v-slot:subtitle>
+        This is a subtitle
+      </template> -->
+
+      <template v-slot:text>
+        <ag-grid-vue
+            style="width: 100%; height: 500px"
+            class="ag-theme-alpine"
+            :columnDefs="columnDefs"
+            :rowData="rowData"
+        >
+        </ag-grid-vue>
+      </template>
+    </v-card>
   </div>
 </template>
 <script>
 /* eslint-disable vue/no-unused-components */
 import { AgGridVue } from "ag-grid-vue3";
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css';
-
-import BtnCellRenderer from './btn-cell-renderer.js';
+import BtnCellRenderer from "./btn-cell-renderer.js";
 
 export default {
   components: {
@@ -27,32 +32,132 @@ export default {
   data() {
     return {
       columnDefs: [
-        { headerName: "id", field: "id", hide: true},
+        { headerName: "id", field: "id", hide: true },
         { headerName: "Discription", field: "disc" },
         { headerName: "Date", field: "date" },
         { headerName: "Start Time", field: "starttime" },
         { headerName: "End Time", field: "endtime" },
         { headerName: "Referance Code", field: "referance" },
-        { headerName: "Action", field: "action", cellRenderer: 'btnCellRenderer',
-        cellRendererParams: {
-          clicked: function(field) {
-            alert(`${field} was clicked`);
-          }
-        }, },
+        {
+          headerName: "Action",
+          field: "action",
+          cellRenderer: "btnCellRenderer",
+          cellRendererParams: {
+            clicked: function (field) {
+              alert(`${field} was clicked`);
+            },
+          },
+        },
       ],
       rowData: [
-        { id: 1, disc: "This albo ought to appear tomorrow", date: "9/1/2021", starttime: "4:30 AM", endtime: "5: 30 AM", referance: 12341234, action: "Action" },
-        { id: 1, disc: "This albo ought to appear tomorrow", date: "9/1/2021", starttime: "4:30 AM", endtime: "5: 30 AM", referance: 12341234, action: "Action" },
-        { id: 1, disc: "This albo ought to appear tomorrow", date: "9/1/2021", starttime: "4:30 AM", endtime: "5: 30 AM", referance: 12341234, action: "Action" },
-        { id: 1, disc: "This albo ought to appear tomorrow", date: "9/1/2021", starttime: "4:30 AM", endtime: "5: 30 AM", referance: 12341234, action: "Action" },
-        { id: 1, disc: "This albo ought to appear tomorrow", date: "9/1/2021", starttime: "4:30 AM", endtime: "5: 30 AM", referance: 12341234, action: "Action" },
-        { id: 1, disc: "This albo ought to appear tomorrow", date: "9/1/2021", starttime: "4:30 AM", endtime: "5: 30 AM", referance: 12341234, action: "Action" },
-        { id: 1, disc: "This albo ought to appear tomorrow", date: "9/1/2021", starttime: "4:30 AM", endtime: "5: 30 AM", referance: 12341234, action: "Action" },
-        { id: 1, disc: "This albo ought to appear tomorrow", date: "9/1/2021", starttime: "4:30 AM", endtime: "5: 30 AM", referance: 12341234, action: "Action" },
-        { id: 1, disc: "This albo ought to appear tomorrow", date: "9/1/2021", starttime: "4:30 AM", endtime: "5: 30 AM", referance: 12341234, action: "Action" },
-        { id: 1, disc: "This albo ought to appear tomorrow", date: "9/1/2021", starttime: "4:30 AM", endtime: "5: 30 AM", referance: 12341234, action: "Action" },
-        { id: 1, disc: "This albo ought to appear tomorrow", date: "9/1/2021", starttime: "4:30 AM", endtime: "5: 30 AM", referance: 12341234, action: "Action" },
-        { id: 1, disc: "This albo ought to appear tomorrow", date: "9/1/2021", starttime: "4:30 AM", endtime: "5: 30 AM", referance: 12341234, action: "Action" },
+        {
+          id: 1,
+          disc: "This albo ought to appear tomorrow",
+          date: "9/1/2021",
+          starttime: "4:30 AM",
+          endtime: "5: 30 AM",
+          referance: 12341234,
+          action: "Action",
+        },
+        {
+          id: 1,
+          disc: "This albo ought to appear tomorrow",
+          date: "9/1/2021",
+          starttime: "4:30 AM",
+          endtime: "5: 30 AM",
+          referance: 12341234,
+          action: "Action",
+        },
+        {
+          id: 1,
+          disc: "This albo ought to appear tomorrow",
+          date: "9/1/2021",
+          starttime: "4:30 AM",
+          endtime: "5: 30 AM",
+          referance: 12341234,
+          action: "Action",
+        },
+        {
+          id: 1,
+          disc: "This albo ought to appear tomorrow",
+          date: "9/1/2021",
+          starttime: "4:30 AM",
+          endtime: "5: 30 AM",
+          referance: 12341234,
+          action: "Action",
+        },
+        {
+          id: 1,
+          disc: "This albo ought to appear tomorrow",
+          date: "9/1/2021",
+          starttime: "4:30 AM",
+          endtime: "5: 30 AM",
+          referance: 12341234,
+          action: "Action",
+        },
+        {
+          id: 1,
+          disc: "This albo ought to appear tomorrow",
+          date: "9/1/2021",
+          starttime: "4:30 AM",
+          endtime: "5: 30 AM",
+          referance: 12341234,
+          action: "Action",
+        },
+        {
+          id: 1,
+          disc: "This albo ought to appear tomorrow",
+          date: "9/1/2021",
+          starttime: "4:30 AM",
+          endtime: "5: 30 AM",
+          referance: 12341234,
+          action: "Action",
+        },
+        {
+          id: 1,
+          disc: "This albo ought to appear tomorrow",
+          date: "9/1/2021",
+          starttime: "4:30 AM",
+          endtime: "5: 30 AM",
+          referance: 12341234,
+          action: "Action",
+        },
+        {
+          id: 1,
+          disc: "This albo ought to appear tomorrow",
+          date: "9/1/2021",
+          starttime: "4:30 AM",
+          endtime: "5: 30 AM",
+          referance: 12341234,
+          action: "Action",
+        },
+        {
+          id: 1,
+          disc: "This albo ought to appear tomorrow",
+          date: "9/1/2021",
+          starttime: "4:30 AM",
+          endtime: "5: 30 AM",
+          referance: 12341234,
+          action: "Action",
+        },
+        {
+          id: 1,
+          disc: "This albo ought to appear tomorrow",
+          date: "9/1/2021",
+          starttime: "4:30 AM",
+          endtime: "5: 30 AM",
+          referance: 12341234,
+          action: "Action",
+        },
+        {
+          id: 1,
+          disc: "This albo ought to appear tomorrow",
+          date: "9/1/2021",
+          starttime: "4:30 AM",
+          endtime: "5: 30 AM",
+          referance: 12341234,
+          action: "Action",
+        },
       ],
     };
   },
@@ -65,4 +170,7 @@ export default {
     },
   },
 };
+
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
 </script>
