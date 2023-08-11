75cf81e5a3e00ca4759d850b85e4b849f356ba0d
Yasir Shaukat <yasirzinbox@gmail.com>
2023-08-11 14:39:29 +0500

    Add dynamic server API

--- src/components/Cards/AppointmentTable.vue
+++ src/components/Cards/AppointmentTable.vue
@@ -19,6 +19,7 @@
 /* eslint-disable vue/no-unused-components */
 import { AgGridVue } from "ag-grid-vue3";
 import BtnCellRenderer from "./btn-cell-renderer.js";
+import API from '@/api'
 
 export default {
   components: {
@@ -29,11 +30,11 @@ export default {
     return {
       columnDefs: [
         { headerName: "id", field: "id", hide: true },
-        { headerName: "Discription", field: "disc" },
-        { headerName: "Date", field: "date" },
-        { headerName: "Start Time", field: "starttime" },
-        { headerName: "End Time", field: "endtime" },
-        { headerName: "Referance Code", field: "referance" },
+        { headerName: "Description", field: "description", width: "540px"},
+        { headerName: "created_at", field: "created_at", dataType: 'DateTime', valueFormatter: (params) => this.dateFormat(params), },
+        // { headerName: "Start Time", field: "starttime" },
+        // { headerName: "End Time", field: "endtime" },
+        { headerName: "Reference Code", field: "slug" },
         {
           headerName: "Action",
           field: "action",
@@ -45,116 +46,117 @@ export default {
           },
         },
       ],
-      rowData: [
-        {
-          id: 1,
-          disc: "This albo ought to appear tomorrow",
-          date: "9/1/2021",
-          starttime: "4:30 AM",
-          endtime: "5: 30 AM",
-          referance: 12341234,
-          action: "Action",
-        },
-        {
-          id: 1,
-          disc: "This albo ought to appear tomorrow",
-          date: "9/1/2021",
-          starttime: "4:30 AM",
-          endtime: "5: 30 AM",
-          referance: 12341234,
-          action: "Action",
-        },
-        {
-          id: 1,
-          disc: "This albo ought to appear tomorrow",
-          date: "9/1/2021",
-          starttime: "4:30 AM",
-          endtime: "5: 30 AM",
-          referance: 12341234,
-          action: "Action",
-        },
-        {
-          id: 1,
-          disc: "This albo ought to appear tomorrow",
-          date: "9/1/2021",
-          starttime: "4:30 AM",
-          endtime: "5: 30 AM",
-          referance: 12341234,
-          action: "Action",
-        },
-        {
-          id: 1,
-          disc: "This albo ought to appear tomorrow",
-          date: "9/1/2021",
-          starttime: "4:30 AM",
-          endtime: "5: 30 AM",
-          referance: 12341234,
-          action: "Action",
-        },
-        {
-          id: 1,
-          disc: "This albo ought to appear tomorrow",
-          date: "9/1/2021",
-          starttime: "4:30 AM",
-          endtime: "5: 30 AM",
-          referance: 12341234,
-          action: "Action",
-        },
-        {
-          id: 1,
-          disc: "This albo ought to appear tomorrow",
-          date: "9/1/2021",
-          starttime: "4:30 AM",
-          endtime: "5: 30 AM",
-          referance: 12341234,
-          action: "Action",
-        },
-        {
-          id: 1,
-          disc: "This albo ought to appear tomorrow",
-          date: "9/1/2021",
-          starttime: "4:30 AM",
-          endtime: "5: 30 AM",
-          referance: 12341234,
-          action: "Action",
-        },
-        {
-          id: 1,
-          disc: "This albo ought to appear tomorrow",
-          date: "9/1/2021",
-          starttime: "4:30 AM",
-          endtime: "5: 30 AM",
-          referance: 12341234,
-          action: "Action",
-        },
-        {
-          id: 1,
-          disc: "This albo ought to appear tomorrow",
-          date: "9/1/2021",
-          starttime: "4:30 AM",
-          endtime: "5: 30 AM",
-          referance: 12341234,
-          action: "Action",
-        },
-        {
-          id: 1,
-          disc: "This albo ought to appear tomorrow",
-          date: "9/1/2021",
-          starttime: "4:30 AM",
-          endtime: "5: 30 AM",
-          referance: 12341234,
-          action: "Action",
-        },
-        {
-          id: 1,
-          disc: "This albo ought to appear tomorrow",
-          date: "9/1/2021",
-          starttime: "4:30 AM",
-          endtime: "5: 30 AM",
-          referance: 12341234,
-          action: "Action",
-        },
-      ],
+      rowData: [],
+    //   rowData: [
+    //     {
+    //       id: 1,
+    //       description: "This albo ought to appear tomorrow",
+    //       created_at: "9/1/2021",
+    //       starttime: "4:30 AM",
+    //       endtime: "5: 30 AM",
+    //       slug: 12341234,
+    //       action: "Action",
+    //     },
+    //     {
+    //       id: 1,
+    //       description: "This albo ought to appear tomorrow",
+    //       created_at: "9/1/2021",
+    //       starttime: "4:30 AM",
+    //       endtime: "5: 30 AM",
+    //       slug: 12341234,
+    //       action: "Action",
+    //     },
+    //     {
+    //       id: 1,
+    //       description: "This albo ought to appear tomorrow",
+    //       created_at: "9/1/2021",
+    //       starttime: "4:30 AM",
+    //       endtime: "5: 30 AM",
+    //       slug: 12341234,
+    //       action: "Action",
+    //     },
+    //     {
+    //       id: 1,
+    //       description: "This albo ought to appear tomorrow",
+    //       created_at: "9/1/2021",
+    //       starttime: "4:30 AM",
+    //       endtime: "5: 30 AM",
+    //       slug: 12341234,
+    //       action: "Action",
+    //     },
+    //     {
+    //       id: 1,
+    //       description: "This albo ought to appear tomorrow",
+    //       created_at: "9/1/2021",
+    //       starttime: "4:30 AM",
+    //       endtime: "5: 30 AM",
+    //       slug: 12341234,
+    //       action: "Action",
+    //     },
+    //     {
+    //       id: 1,
+    //       description: "This albo ought to appear tomorrow",
+    //       created_at: "9/1/2021",
+    //       starttime: "4:30 AM",
+    //       endtime: "5: 30 AM",
+    //       slug: 12341234,
+    //       action: "Action",
+    //     },
+    //     {
+    //       id: 1,
+    //       description: "This albo ought to appear tomorrow",
+    //       created_at: "9/1/2021",
+    //       starttime: "4:30 AM",
+    //       endtime: "5: 30 AM",
+    //       slug: 12341234,
+    //       action: "Action",
+    //     },
+    //     {
+    //       id: 1,
+    //       description: "This albo ought to appear tomorrow",
+    //       created_at: "9/1/2021",
+    //       starttime: "4:30 AM",
+    //       endtime: "5: 30 AM",
+    //       slug: 12341234,
+    //       action: "Action",
+    //     },
+    //     {
+    //       id: 1,
+    //       description: "This albo ought to appear tomorrow",
+    //       created_at: "9/1/2021",
+    //       starttime: "4:30 AM",
+    //       endtime: "5: 30 AM",
+    //       slug: 12341234,
+    //       action: "Action",
+    //     },
+    //     {
+    //       id: 1,
+    //       description: "This albo ought to appear tomorrow",
+    //       created_at: "9/1/2021",
+    //       starttime: "4:30 AM",
+    //       endtime: "5: 30 AM",
+    //       slug: 12341234,
+    //       action: "Action",
+    //     },
+    //     {
+    //       id: 1,
+    //       description: "This albo ought to appear tomorrow",
+    //       created_at: "9/1/2021",
+    //       starttime: "4:30 AM",
+    //       endtime: "5: 30 AM",
+    //       slug: 12341234,
+    //       action: "Action",
+    //     },
+    //     {
+    //       id: 1,
+    //       description: "This albo ought to appear tomorrow",
+    //       created_at: "9/1/2021",
+    //       starttime: "4:30 AM",
+    //       endtime: "5: 30 AM",
+    //       slug: 12341234,
+    //       action: "Action",
+    //     },
+    //   ],
     };
   },
   props: {
@@ -164,6 +166,28 @@ export default {
         return ["light", "dark"].indexOf(value) !== -1;
       },
     },
+  },
+  methods: {
+    dateFormat: function (params) {
+        var date = new Date(params.value);
+        var day = date.getDate().toString().padStart(2, '0');
+        var month = (date.getMonth() + 1).toString().padStart(2, '0');
+        var year = date.getFullYear().toString().substring(2);
+        return day + '/' + month + '/' + year;
+    }
+  },
+  mounted() {
+    API.getAppointments(
+        data => {
+            console.log(data)
+            if (data.length > 0) {
+                this.rowData = data;
+            }
+        },
+        err => {
+            console.log('err :', err);
+        }
+    )
   },
 };
 
